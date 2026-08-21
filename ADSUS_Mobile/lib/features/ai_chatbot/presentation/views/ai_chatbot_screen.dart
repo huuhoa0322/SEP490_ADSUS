e81e3e3764f493b1843a1b3788b4d18a1b1b22a5
Le Duc Anh <anhldhe180218@fpt.edu.vn>
2026-08-21 12:11:07 +0700

    Update ai_chatbot_screen.dart

--- ADSUS_Mobile/lib/features/ai_chatbot/presentation/views/ai_chatbot_screen.dart
+++ ADSUS_Mobile/lib/features/ai_chatbot/presentation/views/ai_chatbot_screen.dart
@@ -441,7 +441,7 @@ class _SuggestionChips extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         padding: const EdgeInsets.symmetric(horizontal: 16),
         itemCount: _suggestions.length,
-        separatorBuilder: (_, __) => const SizedBox(width: 8),
+        separatorBuilder: (_, _) => const SizedBox(width: 8),
         itemBuilder: (context, index) {
           final text = _suggestions[index];
           return GestureDetector(
